fdbf33992e48d3c98de915beb7de573f9e0a97f8
Gabriel Valdivieso <gabriel.valdivieso.005@gmail.com>
2026-02-05 11:19:55 -0500

    Section 30 - Class 423: MapStruct IntelliJ Plugin

--- src/main/java/guru/springfamework/api/v1/mapper/ProductMapper.java
+++ src/main/java/guru/springfamework/api/v1/mapper/ProductMapper.java
@@ -3,10 +3,12 @@ package guru.springfamework.api.v1.mapper;
 import guru.springfamework.api.v1.model.ProductDTO;
 import guru.springfamework.domain.Product;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
 
 @Mapper
 public interface ProductMapper {
     ProductMapper INSTANCE = Mappers.getMapper(ProductMapper.class);
+    @Mapping(source = "id", target = "id")
     ProductDTO productToProductDTO(Product product);
 }
